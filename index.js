2c502e7b780c921968fbf0f8066fb7d45a34f71a
Lucas Candido <lucas.candido@grupomytec.com.br>
2025-04-02 20:23:04 -0300

    Adicionando Endpoint Post

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ server.get("/customers/:id", (req, res) => {
     return res.status(status).json(customer || { error: "Customer not found" });
 });
 
+server.post("/customers", (req, res) => {
+    const { name, site } = req.body;
+    const id = customers[customers.length - 1].id + 1;
+
+    const newCustomer = { id, name, site };
+    customers.push(newCustomer);
+
+    return res.status(201).json(newCustomer);
+});
+
 server.use((req, res) => {
     res.status(404).json({ error: "Not Found" });
 });
